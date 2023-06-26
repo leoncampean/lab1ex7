fd53bd285b5e8e9f57524c80b7dc131b4faf7d71
leoncampean <87850785+leoncampean@users.noreply.github.com>
2023-06-26 18:19:50 +0300

    Add files via upload

---
+++ ee1lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ee1lab1
+{
+    class MainClass
+    {
+        /*
+         * Scrieti un program care va calcula media aritmetica a trei numere citite de la
+         * tastatura
+         */
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("introduceti numarul:");
+
+            int valoareNumarului = int.Parse(Console.ReadLine());
+            var textParSauImpar = "";
+
+            if (valoareNumarului % 2 == 0)
+            {
+                textParSauImpar = " este par";
+            } else
+            {
+                textParSauImpar = " este impar";
+            }
+            Console.WriteLine("numarul " + valoareNumarului + textParSauImpar);
+        }
+    }
+}
+
